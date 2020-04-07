b9830d988d27cc99017f55a0f1634eb8862f5990
Stuart Smith <stuart.smith@toyotaconnected.eu>
2020-04-07 09:36:45 +0100

    pushing latest, edit an available bed and ui updates, keeping the edited bed in the correct index

--- app/controllers/ward/index.js
+++ app/controllers/ward/index.js
@@ -1,8 +1,12 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import UpdateBed from 'bed-checker/gql/mutations/update-bed';
 
 export default class WardController extends Controller {
+  @service apollo;
+
   @tracked totalBeds = this.model.totalBeds || 0;
   @tracked availableBeds = this.model.availableBeds || 0;
   @tracked totalVentilatorInUse = this.model.totalVentilatorInUse || 0;
@@ -11,6 +15,7 @@ export default class WardController extends Controller {
   @tracked totalCovidStatusSuspected = this.model.totalCovidStatusSuspected || 0;
   @tracked editBedModalIsOpen = false;
   @tracked bedInMemory = null;
+  @tracked changesMade = false;
 
   get availableBedsPercentage() {
     if (this.totalBeds === 0) {
@@ -22,8 +27,10 @@ export default class WardController extends Controller {
 
   @action
   openEditBedModal(bed, index) {
-    this.bedInMemory = bed;
-    this.set('bedInMemory', { index: index + 1 });
+    this.bedInMemory = {
+      bed,
+      index: index + 1
+    }
     this.editBedModalIsOpen = true;
   }
 
@@ -31,14 +38,63 @@ export default class WardController extends Controller {
   closeModal() {
     this.editBedModalIsOpen = false;
     this.bedInMemory = null;
+    this.changesMade = false;
   }
 
-  // @action
-  // setCovidStatus(status) {
-  //   if (status === 'positive') {
-  //     this.setProperties('bedInMemory', {
+  @action
+  setCovidStatus(status) {
+    this.set('bedInMemory', { 
+      bed: { 
+        available: this.bedInMemory.bed.available,
+        covidStatus: status,
+        id: this.bedInMemory.bed.id,
+        ventilatorInUse: this.bedInMemory.bed.ventilatorInUse
+      } 
+    });
+    this.changesMade = true;
+  }
 
-  //     });
-  //   }
-  // }
+  @action
+  setVentilatorStatus() {
+    this.set('bedInMemory', { 
+      bed: { 
+        available: this.bedInMemory.bed.available,
+        covidStatus: this.bedInMemory.bed.covidStatus,
+        id: this.bedInMemory.bed.id,
+        ventilatorInUse: !this.bedInMemory.bed.ventilatorInUse
+      }
+    });
+    this.changesMade = true;
+  }
+
+  @action
+  async editBed(available, event) {
+    event.preventDefault();
+
+    this.error = false;
+
+    const variables = {
+      input: {
+        available,
+        covidStatus: this.bedInMemory.bed.covidStatus,
+        id: this.bedInMemory.bed.id,
+        ventilatorInUse: this.bedInMemory.bed.ventilatorInUse
+      }
+    };
+
+    try {
+      const { updateBed } = await this.apollo.mutate({ mutation: UpdateBed, variables });
+
+      const mutatableBedsArray = [...this.model.beds];
+      const foundIndex = mutatableBedsArray.findIndex(x => x.id === updateBed.bed.id);
+      mutatableBedsArray[foundIndex] = updateBed.bed;
+
+      this.set('model.beds', mutatableBedsArray);
+      this.closeModal();
+      // this.set('model.showSuccessMessage', true);
+    } catch (error) {
+      this.error = true;
+      console.error(error);
+    }
+  }
 }
